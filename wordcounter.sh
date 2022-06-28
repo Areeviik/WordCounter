a44fa35f2d9590be84797e7b3eb45a4f5f2be80c
Arevik <arevik.xachaturyan@gmail.com>
2022-06-29 01:26:30 +0400

    without -l option

--- wordcounter.sh
+++ wordcounter.sh
@@ -1,17 +1,41 @@
 #! /bin/bash
 
-if [ $#==0 ]
+args=("$@")
+if [ $# -eq 0 ]
 then
 	echo "Wrong number of arguments"
 	echo "Program takes at least one argument: filename"
 	echo "And three more optional arguments: [-w|-c|-l]"
 
-fi
-if [ $#==1 ]
+elif [ $# -eq 1 ]
 then
 	file=$1
-	word_count=`wc -w < $file`
-	char_count=`wc -c < $file`
+	word_count=`wc -w < "$file"`
+	char_count=`wc -c < "$file"`
 	echo "Total words count: $word_count"
 	echo "Total characters count: $char_count"
 fi
+
+i=1
+while [ $i -le $# ]
+do
+	file=$1
+	if [ ${args[${i}]}="-w" ]
+	then
+		after_opt=$(( $i+1 ))
+		search_word=${args[${after_opt}]}
+		search_word_count=`grep "$search_word" "$file" | wc -l`
+		echo "Occurence of word $search_word : $search_word_count"
+
+	elif [ ${args[${i}]}="-c" ]
+	then
+		after_opt=$(( $i+1 ))
+		search_char=${args[${after_opt}]}
+		search_char_count=`grep "$search_char" "$file" | wc -l`
+		echo "Occurence of word $search_char: $search_char_count"
+	fi
+	
+	i=$(( $i+1 ))
+done
+
+
