210c6ab4e5a89c41ec4d3ad7cea097f9fd7db81f
Arevik <arevik.xachaturyan@gmail.com>
2022-07-01 21:01:24 +0400

    done

--- wordcounter.sh
+++ wordcounter.sh
@@ -9,76 +9,79 @@ then
 
 
 else
-	l_flag=false
+	l_flag=0
 	i=2
+	file=$2
 	while [ $i -le $# ]
 	do
 		opt_l="-l"
+		opt_w="-w"
+		opt_c="-c"
 		cur_arg=${args[${i}]}
 		if [ "$cur_arg" = "$opt_l" ]
 		then
-			l_flag=true
+			l_flag=1
+		elif [ "$cur_arg" = "$opt_w" ]; then
+			w_flag=1
+			after_opt_w=$(( $i+1 ))
+			search_word=${args[${after_opt_w}]}
+			search_word_count=`grep "$search_word" "$file" | wc -l`
+
+		elif [ "$cur_arg" = "$opt_c" ]; then
+			c_flag=1
+			after_opt_c=$(( $i+1 ))
+			search_char=${args[${after_opt_c}]}
+			search_char_count=`grep "$search_char" "$file" | wc -l`
 		fi
 		i=$(( $i+1 ))
 	done
-	file=$2
-	if [ !$l_flag ]
+
+	if [ $l_flag -eq 0 ]
 	then
-		echo "l_flag : $l_flag"
 		word_count=`wc -w < "$file"`
 		char_count=`tr -d '[:space:]' < "$file" | wc -c`
 		echo "Total words count: $word_count"
 		echo "Total characters count: $char_count"
 	fi
-i=2
-while [ $i -le $# ]
-do
-	file=$2
-	opt_w="-w"
-	opt_c="-c"
-	opt_l="-l"
-	cur_arg=${args[${i}]}
+	i=2
+	while [ $i -le $# ]
+	do
+		cur_arg=${args[${i}]}
 
-	if [ "$cur_arg" = "$opt_w" ];
-	then
-		if [ !$l_flag ]
+		if [ "$cur_arg" = "$opt_w" ];
 		then
-			w_flag=true
+			w_flag=1
+			if [ $l_flag -eq 0 ]
+			then
+				echo "Occurence of word $search_word : $search_word_count"
+			fi
+		elif [ "$cur_arg" = "$opt_c" ];
+		then
+			c_flag=1
+			if [ "$l_flag" -eq 0 ]
+			then
+				echo "Occurence of character $search_char: $search_char_count"
+			fi
+		elif [ "$cur_arg" = "$opt_l" ];
+		then
+			l_flag=1
 			after_opt=$(( $i+1 ))
-			search_word=${args[${after_opt}]}
-			search_word_count=`grep "$search_word" "$file" | wc -l`
-			echo "Occurence of word $search_word : $search_word_count"
+			output_file=${args[${after_opt}]}
+			word_count=`wc -w < "$file"`
+			char_count=`tr -d '[:space:]' < "$file" | wc -c`
+			echo "Output was written in file named [$output_file]"
+			`echo "Total words count: $word_count" >> "$output_file"`
+			`echo "Total characters count: $char_count" >> "$output_file"`
+			if [ $w_flag ]
+			then
+				`echo "Occurence of word $search_word : $search_word_count" >> "$output_file"`
+			fi
+			if [ $c_flag ]
+			then
+				`echo "Occurence of character $search_char: $search_char_count" >> "$output_file"`
+			fi
 		fi
-	elif [ "$cur_arg" = "$opt_c" ];
-	then
-		if [ !$l_flage ]
-		then
-			c_flag=true
-			after_opt=$(( $i+1 ))
-			search_char=${args[${after_opt}]}
-			search_char_count=`grep "$search_char" "$file" | wc -l`
-			echo "Occurence of character $search_char: $search_char_count"
-		fi
-	elif [ "$cur_arg" = "$opt_l" ];
-	then
-		l_flag=true
-		after_opt=$(( $i+1 ))
-		output_file=${args[${after_opt}]}
-		word_count=`wc -w < "$file"`
-		char_count=`tr -d '[:space:]' < "$file" | wc -c`
-		echo "Output was written in file named [$output_file]"
-		`echo "Total words count: $word_count" >> "$output_file"`
-		`echo "Total characters count: $char_count" >> "$output_file"`
-		if [ $w_flag ]
-		then
-			`echo "Occurence of word $search_word : $search_word_count" >> "$output_file"`
-		fi
-		if [ $c_flag ]
-		then
-			`echo "Occurence of character $search_char: $search_char_count" >> "$output_file"`
-		fi
+		i=$(( $i+1 ))
+	done
+
 	fi
-	i=$(( $i+1 ))
-done
-
-fi
